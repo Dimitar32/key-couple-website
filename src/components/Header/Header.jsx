da8e1dbae3aa219a67c9ada38ad46983edb2f40e
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2024-11-25 14:11:14 +0200

    change question mark

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,6 @@ const Header = () => {
     return (
         <header className="header">
             <h1>KeyCouple8</h1>
-            {/*<h1><a href="#general-info" >Disney Keychains</a></h1>*/}
             <nav>
                 <Link to="/">Продукти</Link>
                 <Link to="/faq">Често задавани въпроси</Link>
