301ae07f414f30dbe44dfb9586e649d2cdd7b684
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2024-12-05 09:53:03 +0200

    add new better product descriptions

--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -22,9 +22,9 @@ const products = [
 const blurProducts = [
     // { id: 1, name: 'Ерик и Ариел', oldPrice: '35.99 лв', price: 'Очаквайте скоро', imageUrl: Ariel },
     // { id: 7, name: 'KeyCouple8 Коледен комплект', oldPrice: '59.90 лв', price: 'Очаквайте скоро', imageUrl: ChristmasQuestionMark},
-    { id: 3, name: 'Рапунцел и Флин', oldPrice: '??.?? лв', price: 'Очаквайте скоро', imageUrl: Rapunzel },
+    { id: 3, name: 'Рапунцел и Флин', oldPrice: '??.?? лв', price: 'Изчерпано', imageUrl: Rapunzel },
     // { id: 4, name: 'Нала и Симба', oldPrice: '35.99 лв', price: 'Очаквайте скоро', imageUrl: LionKing },
-    { id: 5, name: 'Бъгс и Лола', oldPrice: '??.?? лв', price: 'Очаквайте скоро', imageUrl: Bunny }
+    { id: 5, name: 'Бъгс и Лола', oldPrice: '??.?? лв', price: 'Изчерпано', imageUrl: Bunny }
     // { id: 6, name: 'Бел и Звяр', oldPrice: '35.99 лв', price: 'Очаквайте скоро', imageUrl: Bella }
 ];
 
