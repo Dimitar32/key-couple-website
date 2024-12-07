448f223248cad2478d37f7d396b57774cec2d184
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2024-12-07 16:01:20 +0200

    add link on title of the page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import './Header.css';
 const Header = () => {
     return (
         <header className="header">
-            <h1>KeyCouple8</h1>
+            <Link to="/" className="logo"><h1>KeyCouple8</h1></Link>
             <nav>
                 <Link to="/">Продукти</Link>
                 <Link to="/faq">Често задавани въпроси</Link>
